21715a3cc668ecb4979fe275105081b276675c9a
Brian Ketelsen <mail@bjk.fyi>
2019-08-07 20:50:09 -0400

    countdown

--- js/script.js
+++ js/script.js
@@ -470,7 +470,7 @@
         = COUNTDOWN CLOCK
     -------------------------------------------*/
     if ($("#clock").length) {
-        $('#clock').countdown('2017/12/25', function(event) {
+        $('#clock').countdown('2019/12/31', function(event) {
             var $this = $(this).html(event.strftime(''
             + '<div class="box"><div>%D</div> <span>Days</span> </div>'
             + '<div class="box"><div>%H</div> <span>Hours</span> </div>'
